5898977a6173ff8356030798cdd10d298ad346b3
LAICHEEHOONG <laicheehoong@gmail.com>
2024-03-11 14:34:08 +0800

    to workspace

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from "../components/Navbar";
+
+
+const HomePage = () => {
+
+    return (
+        <div className="home-page-container">
+
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ src/pages/PriceTagPage.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from "../components/Navbar";
+
+const PriceTagPage = () => {
+
+    return (
+        <div className="home-page-container">
+   
+        </div>
+    )
+}
+
+export default PriceTagPage;
